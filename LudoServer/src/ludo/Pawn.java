5456ca5ebc13e6d181cc70590042c651a6df7a4c
udedovic <58147527+udedovic@users.noreply.github.com>
2020-03-17 14:00:26 +0100

    Revert "prvi put promenjeno"

--- LudoServer/src/ludo/Pawn.java
+++ LudoServer/src/ludo/Pawn.java
@@ -5,40 +5,14 @@ public class Pawn {
 	private String naziv;
 	private Color color;
 	private int index;
-	private int coordinatePawn_x;
-	private int coordinatePawn_y;	
 	
-
-	public Pawn(String naziv, Color color, int index, int coordinatePawn_x, int coordinatePawn_y) {
+	
+	public Pawn(String naziv, Color color, int index) {
 		super();
 		this.naziv = naziv;
 		this.color = color;
 		this.index = index;
-		this.coordinatePawn_x = coordinatePawn_x;
-		this.coordinatePawn_y = coordinatePawn_y;
 	}
-	
-
-	public int getCoordinatePawn_x() {
-		return coordinatePawn_x;
-	}
-
-
-	public void setCoordinatePawn_x(int coordinatePawn_x) {
-		this.coordinatePawn_x = coordinatePawn_x;
-	}
-
-
-	public int getCoordinatePawn_y() {
-		return coordinatePawn_y;
-	}
-
-
-	public void setCoordinatePawn_y(int coordinatePawn_y) {
-		this.coordinatePawn_y = coordinatePawn_y;
-	}
-
-
 
 	public String getNaziv() {
 		return naziv;
