d93f6cbebce9d8c53c8bacf07ef6b6c8b086eab9
Dejan Petrovic <dejanpetrovic885@gmail.com>
2020-03-18 01:56:14 +0100

    p3

--- ludo client/src/ludoClient/Command.java
+++ ludo client/src/ludoClient/Command.java
@@ -2,8 +2,6 @@ package ludoClient;
 
 public class Command {
 	
-	public static final int EVRYTHING_OK_REFRESH = 0;
-	
 	public static final int THROW_DICE = 10;
 	public static final int PLAYER_IS_READY = 11;
 
